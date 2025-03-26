8e57d2cb3cf9136523caea44a4045235126e4a47
Vipertech (Affolter Matias) <74778466+vipertechofficial@users.noreply.github.com>
2025-03-26 04:03:55 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -26,10 +26,10 @@ var LOAD_FILES_USEFUL = [];
 var LOAD_FILES_STATIC = [];
 
 // Cache names
-var REQUIRED_CACHE = "unless-update-cache-v45-required";
-var USEFUL_CACHE = "unless-update-cache-v45-useful";
-var STATIC_CACHE = "unless-update-cache-v45-static";
-var OTHER_CACHE = "unless-update-cache-v45-other";
+var REQUIRED_CACHE = "unless-update-cache-v46-required";
+var USEFUL_CACHE = "unless-update-cache-v46-useful";
+var STATIC_CACHE = "unless-update-cache-v46-static";
+var OTHER_CACHE = "unless-update-cache-v46-other";
 
 // Regular expressions for chunk matching
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-zA-Z0-9_-]+)\.min\.js$/i;
