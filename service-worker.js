bede22e4bce2bc7d2d85bc13974747589ad10c2e
Vipertech (Affolter Matias) <74778466+vipertechofficial@users.noreply.github.com>
2025-12-29 21:36:26 +0100

    Ok let's go

--- service-worker.js
+++ service-worker.js
@@ -26,10 +26,10 @@ var LOAD_FILES_USEFUL = [];
 var LOAD_FILES_STATIC = [];
 
 // Cache names
-var REQUIRED_CACHE = "unless-update-cache-v132-required";
-var USEFUL_CACHE = "unless-update-cache-v132-useful";
-var STATIC_CACHE = "unless-update-cache-v132-static";
-var OTHER_CACHE = "unless-update-cache-v132-other";
+var REQUIRED_CACHE = "unless-update-cache-v133-required";
+var USEFUL_CACHE = "unless-update-cache-v133-useful";
+var STATIC_CACHE = "unless-update-cache-v133-static";
+var OTHER_CACHE = "unless-update-cache-v133-other";
 
 // Regular expressions for chunk matching
 var MAIN_CHILD_CHUNK_REGEX = /chunk_(main_[a-zA-Z0-9_-]+)\.min\.js$/i;
