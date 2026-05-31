6d8edffb9a96b6ea2ab2c15adab745b862ed53af
Matias Affolter <matiasaffolter@gmail.com>
2026-05-31 18:30:34 +0200

    Update

--- service-worker.js
+++ service-worker.js
@@ -47,10 +47,10 @@ var LOAD_FILES_USEFUL = [];
 var LOAD_FILES_STATIC = [];
 
 // Cache names
-var REQUIRED_CACHE = "unless-update-cache-v186-required";
-var USEFUL_CACHE = "unless-update-cache-v186-useful";
-var STATIC_CACHE = "unless-update-cache-v186-static";
-var OTHER_CACHE = "unless-update-cache-v186-other";
+var REQUIRED_CACHE = "unless-update-cache-v187-required";
+var USEFUL_CACHE = "unless-update-cache-v187-useful";
+var STATIC_CACHE = "unless-update-cache-v187-static";
+var OTHER_CACHE = "unless-update-cache-v187-other";
 var ALL_CACHES = [REQUIRED_CACHE, STATIC_CACHE, USEFUL_CACHE, OTHER_CACHE];
 
 // Regular expressions for chunk matching
