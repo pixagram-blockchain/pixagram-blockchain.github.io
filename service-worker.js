b0e6b93e8201825fe81919a8aedcc1a485693592
Matias Affolter <matiasaffolter@gmail.com>
2026-06-05 16:40:54 +0200

    Wallet update
    Added functionalities such as savings and recurring transfer as well as pending swap.

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ var LOAD_FILES_USEFUL = [];
 var LOAD_FILES_STATIC = [];
 
 // Cache names
-var V = "v192";
+var V = "v193";
 var REQUIRED_CACHE = "unless-update-cache-"+V+"-required";
 var USEFUL_CACHE = "unless-update-cache-"+V+"-useful";
 var STATIC_CACHE = "unless-update-cache-"+V+"-static";
