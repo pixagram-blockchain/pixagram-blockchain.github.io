f0f246f59315a785d72bc31d6ec698790c0e95a5
Matias Affolter <matiasaffolter@gmail.com>
2026-06-17 15:51:54 +0200

    Jun 17
    particle effect on posting

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ var LOAD_FILES_USEFUL = [];
 var LOAD_FILES_STATIC = [];
 
 // Cache names
-var V = "v197";
+var V = "v198";
 var REQUIRED_CACHE = "unless-update-cache-"+V+"-required";
 var USEFUL_CACHE = "unless-update-cache-"+V+"-useful";
 var STATIC_CACHE = "unless-update-cache-"+V+"-static";
