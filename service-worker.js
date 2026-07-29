b0642faf7a23ebc48b5f2ae2ad519aaffd273223
Matias Affolter <matiasaffolter@gmail.com>
2026-07-29 23:58:07 +0200

    Jul 29

--- service-worker.js
+++ service-worker.js
@@ -32,7 +32,7 @@ var LOAD_FILES_USEFUL = [];
 var LOAD_FILES_STATIC = [];
 
 // Cache names
-var V = "v216";
+var V = "v217";
 var REQUIRED_CACHE = "unless-update-cache-"+V+"-required";
 var USEFUL_CACHE = "unless-update-cache-"+V+"-useful";
 var STATIC_CACHE = "unless-update-cache-"+V+"-static";
